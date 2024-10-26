04965160115537bc3319e385467805f898fd2ece
Robert Massaioli <rmassaioli@atlassian.com>
2024-10-26 12:57:27 +1100

    Getting to the point where we can test everything out fairly well.
    The second that it start working locally I'll begin to create a much
    better test framework for it.
    
    I think that I might just to testing in storybooks.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export type MapshotSaves = {
+  [saveName: string]: Array<MapshotSave>;
+};
+
+export type MapshotSave = {
+  savename: string;
+  unique_id: string;
+  ticks_played: number;
+};
+
 function App() {
+  const [saves, setSaves] = useState<MapshotSaves | undefined>(undefined);
+
+  useEffect(() => {
+    const load = async () => {
+      const rawResult = await fetch(`${process.env.PUBLIC_URL}/mapshot/saves.json`);
+      setSaves(await rawResult.json());
+    };
+
+    load();
+  }, []);
+
   return (
     <div className="App">
+      <h1>Mapshot Map Directory</h1>
+      <p>Welcome to the mapshot directory, browse any save you like and see all of the generated maps for that save as it progresses over time.</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
