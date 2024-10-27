ea760ade2eb0a2c72cf50de7286872a55a56480b
Robert Massaioli <rmassaioli@atlassian.com>
2024-10-27 11:12:53 +1100

    Adding in Material UI.
    Now I can give it some nice styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import { MapshotSave } from './data';
 import { SaveLoader } from './SaveLoader';
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Header from './Header';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Mapshot Map Directory</h1>
-      <p>Welcome to the mapshot directory, browse any save you like and see all of the generated maps for that save as it progresses over time.</p>
-      <SaveLoader />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <div className="App">
+        <h1>Mapshot Map Directory</h1>
+        <p>Welcome to the mapshot directory, browse any save you like and see all of the generated maps for that save as it progresses over time.</p>
+        <SaveLoader />
+      </div>
+    </ThemeProvider>
   );
 }
 
